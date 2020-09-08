a665f685b552f05a11b8b45b95049427000045b9
palwinderg <70971434+palwinderg@users.noreply.github.com>
2020-09-08 14:51:43 -0400

    Add files via upload

---
+++ Obstacle_Avoidance_Robot.ino
@@ -0,0 +1,33 @@
+
+
+//                                                                              OBSTACLE AVOIDANCE ROBOT
+//**********************************************************************************************************************************************************************************
+// Created By: Palwinder S. Gill
+// Created On: 27 July, 2020
+// Synopsis: This code will let a robot know if an obstacle is in the way and tell it to stop and change direction
+//**********************************************************************************************************************************************************************************
+
+
+//variables for the ultrasonic sensor
+const int trigPin = 11;                         //variable for the trigger on ultrasonic sensor
+const int echoPin = 10;                         //variable for the echo reciever on ultrasonic sensor
+
+//variables for motor 1 (left leg)
+const int m1Dir1Pin = 4;                        //this is the pin for direction pin 1 of motor 1
+const int m1Dir2Pin = 5;                        //this is the pin for direction pin 2 of motor 2
+const int m1SpeedPin = 6;                       //pin for speed of motor 1
+
+//variables for motor 2 (right leg)
+const int m2Dir1Pin = 7;                        //pin for direction pin 1 of motor 1
+const int m2Dir1Pin = 8;                        //pin for direction pin 2 of motor 2
+const int m2Dir1Pin = 9;                        //pin for speed of motor 2
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
